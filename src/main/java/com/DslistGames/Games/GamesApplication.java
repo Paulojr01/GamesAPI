03d8f307b1e32e5f2176308447e62c6ae49cb7a9
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-05-09 00:12:20 -0300

    Initial commit

---
+++ src/main/java/com/DslistGames/Games/GamesApplication.java
@@ -0,0 +1,13 @@
+package com.DslistGames.Games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesApplication.class, args);
+	}
+
+}

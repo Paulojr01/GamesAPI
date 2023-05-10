661379039ffbbb6e1761b6c3faea542ade5878e6
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-05-10 02:56:16 -0300

    Criado o end point get By id

--- src/main/java/com/DslistGames/Games/controllers/GamesController.java
+++ src/main/java/com/DslistGames/Games/controllers/GamesController.java
@@ -1,10 +1,13 @@
 package com.DslistGames.Games.controllers;
 
+import com.DslistGames.Games.dto.GamesDTO;
 import com.DslistGames.Games.dto.GamesMinDTO;
 import com.DslistGames.Games.entities.Games;
 import com.DslistGames.Games.service.GamesService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,10 @@ public class GamesController {
         return result;
     }
 
+    @GetMapping(value = "/{id}")
+    public GamesDTO findById(@PathVariable Long id){
+        GamesDTO result = gamesService.findById(id);
+        return result;
+    }
 
 }
--- src/main/java/com/DslistGames/Games/service/GamesService.java
+++ src/main/java/com/DslistGames/Games/service/GamesService.java
@@ -1,11 +1,13 @@
 package com.DslistGames.Games.service;
 
 
+import com.DslistGames.Games.dto.GamesDTO;
 import com.DslistGames.Games.dto.GamesMinDTO;
 import com.DslistGames.Games.entities.Games;
 import com.DslistGames.Games.repository.GamesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -15,8 +17,15 @@ public class GamesService {
     @Autowired
     private GamesRepository gamesRepository;
 
+
+
     public List<GamesMinDTO> findAll() {
         List<Games> result = gamesRepository.findAll();
         return result.stream().map(GamesMinDTO::new).toList();
     }
+
+    public GamesDTO findById(Long id){
+        Games result = gamesRepository.findById(id).get();
+        return  new GamesDTO(result);
+   }
 }
